5963bc06931b4c2f852c5778a0e5beae6fdb4257
Sage Weil <sage@redhat.com>
2015-11-17 13:18:02 -0500

    Revert "osd: reduce actual size of pg_log_entry_t"
    This reverts commit 4b2d07e3f6402f9df6d6102d225e42f35b9daad5.

--- src/osd/ReplicatedPG.cc
+++ src/osd/ReplicatedPG.cc
@@ -3333,10 +3333,7 @@ ReplicatedPG::RepGather *ReplicatedPG::trim_object(const hobject_t &coid)
       ctx->log.back().mod_desc.mark_unrollbackable();
     }
     
-    if (!ctx->log.back().snaps) {
-      ctx->log.back().snaps = new bufferlist();
-    }
-    ::encode(coi.snaps, *(ctx->log.back().snaps));
+    ::encode(coi.snaps, ctx->log.back().snaps);
     ctx->at_version.version++;
   }
 
@@ -6182,10 +6179,7 @@ void ReplicatedPG::make_writeable(OpContext *ctx)
 				      ctx->obs->oi.version,
 				      ctx->obs->oi.user_version,
 				      osd_reqid_t(), ctx->new_obs.oi.mtime));
-    if (!ctx->log.back().snaps) {
-      ctx->log.back().snaps = new bufferlist();
-    }
-    ::encode(snaps, *(ctx->log.back().snaps));
+    ::encode(snaps, ctx->log.back().snaps);
     ctx->log.back().mod_desc.create();
 
     ctx->at_version.version++;
@@ -6579,10 +6573,7 @@ void ReplicatedPG::finish_ctx(OpContext *ctx, int log_op_type, bool maintain_ssc
     case pg_log_entry_t::CLEAN:
       dout(20) << __func__ << " encoding snaps " << ctx->new_obs.oi.snaps
 	       << dendl;
-      if (!ctx->log.back().snaps) {
-        ctx->log.back().snaps = new bufferlist();
-      }
-      ::encode(ctx->new_obs.oi.snaps, *(ctx->log.back().snaps));
+      ::encode(ctx->new_obs.oi.snaps, ctx->log.back().snaps);
       break;
     default:
       break;
